6744afca69c4f6b67ceb40fbab41826e26c78034
nooshindev <nooshi.jamshidian@gmail.com>
2025-10-29 21:58:46 +0330

    calcute power of two numbers

---
+++ power.py
@@ -0,0 +1,10 @@
+number = input("enter two-digit number:")
+
+a = int(number[0])
+b = int(number[1])
+
+result1 = a ** b
+result2 = b ** a
+
+print("first number to the power of second number:: ", result1)
+print("first number to the power of second number:: ", result2)
